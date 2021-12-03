6899452d30853bf62b4a3d3de7e3d3bcebd51bcd
Penny Yu <pennyyohah@outlook.com>
2021-12-03 10:09:50 +0800

    add interpretion of the methods

--- score_integrated.py
+++ score_integrated.py
@@ -6,18 +6,16 @@ from stumpy import stump
 
 small_quantile = 0.1
 
-# 'orig_p2p_inv','acc_std_inv',
-
 names = [
-    'orig_p2p',
-    'diff_p2p',
-    'acc_p2p',
-    'orig_p2p_inv',
-    'diff_small',
-    'diff_large',
-    'diff_cross',
-    'tukey_test_min',
-    'tukey_test_max',
+    'orig_p2p',  # Extreme Value Detection
+    'diff_p2p',  # Extreme Differenced Value Detection
+    'acc_p2p',  # Extreme Second Order Differenced Value Detection
+    'orig_p2p_inv',  # Inversion of Extreme Value Detection
+    'diff_small',  # Outlier Detection Based on Quantile Range
+    'diff_large',  # Outlier Detection Based on Quantile Range
+    'diff_cross',  # Auto-correlation Detection of Differenced Value
+    'tukey_test_min',  # Outlier Detection Based on Tukey’s Test
+    'tukey_test_max',  # Outlier Detection Based on Tukey’s Test
     'acc_std',
     'acc_std_inv',
     'orig_mp_novelty',
@@ -33,12 +31,14 @@ def orig_p2p(seq, w):
     seq['orig_p2p'] = (rolling_max - rolling_min).shift(-w)
     return seq
 
+
 def diff_p2p(seq, w):
     seq['diff'] = seq['orig'].diff(1)
     rolling_max = seq['diff'].rolling(w).max()
     rolling_min = seq['diff'].rolling(w).min()
     seq['diff_p2p'] = (rolling_max - rolling_min).shift(-w)
     return seq
+
 
 def acc_p2p(seq, w):
     seq['acc'] = seq['diff'].diff(1)
@@ -47,10 +47,12 @@ def acc_p2p(seq, w):
     seq['acc_p2p'] = (rolling_max - rolling_min).shift(-w)
     return seq
 
+
 def orig_p2p_inv(seq, w):
     numer = seq['orig_p2p'].mean()
     seq['orig_p2p_inv'] = numer / seq['orig_p2p']
     return seq
+
 
 def diff_small(seq, w):
     diff_abs = seq['diff'].abs()
@@ -58,11 +60,13 @@ def diff_small(seq, w):
     seq['diff_small'] = cond.rolling(w).mean().shift(-w)
     return seq
 
+
 def diff_large(seq, w):
     diff_abs = seq['diff'].abs()
     cond = diff_abs > diff_abs.quantile(small_quantile)
     seq['diff_large'] = cond.rolling(w).mean().shift(-w)
     return seq
+
 
 def tukey_test_min(seq, w):
     q1 = seq['diff'].rolling(w).quantile(0.25)
@@ -71,6 +75,7 @@ def tukey_test_min(seq, w):
     seq['tukey_test_min'] = ((q1 - min) / (q3 - q1)).shift(-w)
     return seq
 
+
 def tukey_test_max(seq, w):
     q1 = seq['diff'].rolling(w).quantile(0.25)
     q3 = seq['diff'].rolling(w).quantile(0.75)
@@ -78,19 +83,23 @@ def tukey_test_max(seq, w):
     seq['tukey_test_max'] = ((max - q3) / (q3 - q1)).shift(-w)
     return seq
 
+
 def diff_cross(seq, w):
     cond = seq['diff'] * seq['diff'].shift(1) < 0
     seq['diff_cross'] = cond.rolling(w).mean().shift(-w)
     return seq
 
+
 def acc_std(seq, w):
     seq['acc_std'] = seq['acc'].rolling(w).std().shift(-w)
     return seq
+
 
 def acc_std_inv(seq, w):
     numer = seq['acc_std'].mean()
     seq['acc_std_inv'] = numer / seq['acc_std']
     return seq
+
 
 def mp_novelty(seq, w, split):
     mp_train = stump(seq['orig'][:split], w)
@@ -132,7 +141,7 @@ def run(X, w, split):
     seq = acc_std_inv(seq, w)
     seq = diff_small(seq, w)
     seq = diff_large(seq, w)
-    seq = tukey_test_min(seq,w)
+    seq = tukey_test_min(seq, w)
     seq = tukey_test_max(seq, w)
     seq = diff_cross(seq, w)
     seq = mp_novelty(seq, w, split)
@@ -152,6 +161,7 @@ def get_score(seq, w):
 
     return seq
 
+
 # Parameter setting
 def init_param():
     min_window_size = 50
@@ -160,6 +170,7 @@ def init_param():
     train_length = 10
     return min_window_size, max_window_size, growth_rate, train_length
 
+
 # Determine window sizes
 def get_window_size():
     size = int(np.log(max_window_size / min_window_size) / np.log(growth_rate)) + 1
@@ -167,12 +178,18 @@ def get_window_size():
     ws = (min_window_size * rates).astype(int)
     return ws
 
+
 # Save results
 def save_result(result):
-    save_results = pd.DataFrame(result, columns=['number', 'w', 'name', 'rate', 'begin', 'end', 'index1', 'value1', 'index2', 'value2'])
-    submission = save_results.loc[save_results.groupby('number')['rate'].idxmax(), ['number','w', 'name', 'rate', 'begin', 'end', 'index1', 'value1', 'index2', 'value2']]
+    save_results = pd.DataFrame(result,
+                                columns=['number', 'w', 'name', 'rate', 'begin', 'end', 'index1', 'value1', 'index2',
+                                         'value2'])
+    submission = save_results.loc[
+        save_results.groupby('number')['rate'].idxmax(), ['number', 'w', 'name', 'rate', 'begin', 'end', 'index1',
+                                                          'value1', 'index2', 'value2']]
     submission.index.number = 'No.'
     submission.to_csv('result.csv')
+
 
 # Path setting
 import pathlib
@@ -249,4 +266,3 @@ for txt_filepath in sorted(txt_dirpath.iterdir()):
 
 # Display results
 save_result(results)
-
